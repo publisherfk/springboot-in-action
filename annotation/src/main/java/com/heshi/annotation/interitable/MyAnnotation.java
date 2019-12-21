a21061faa25739173665e02a05c8a01de4130210
傅昆 <fukun@supcon.com>
2019-12-21 16:18:05 +0800

    增加注解的示例代码

---
+++ annotation/src/main/java/com/heshi/annotation/interitable/MyAnnotation.java
@@ -0,0 +1,12 @@
+package com.heshi.annotation.interitable;
+
+import java.lang.annotation.*;
+
+/**
+ * @author by fukun
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface MyAnnotation {
+}
