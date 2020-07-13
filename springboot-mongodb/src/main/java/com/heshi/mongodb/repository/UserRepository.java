c875700f770b3fdef8e1f83ba4e8e59a98271ebd
傅昆 <fukun@supcon.com>
2020-07-13 18:45:58 +0800

    删除springboot-mongodb

--- springboot-mongodb/src/main/java/com/heshi/mongodb/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.heshi.mongodb.repository;
-
-import com.heshi.mongodb.DO.UserDO;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<UserDO, Integer> {
-}
